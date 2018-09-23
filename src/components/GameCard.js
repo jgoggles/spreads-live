af00f5ca8e473914703600173291385bc98453c4
Dan Weaver <daniel.weaver@gmail.com>
2018-09-22 19:38:29 -0600

    logos

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
+import './Scores.css';
 
 class GameCard extends Component {
   render() {
     const { game } = this.props;
     const scoreWidth = {width: '40px'};
     return (
-      <Table bordered condensed>
+      <Table className="score" bordered condensed>
         <tbody>
           <tr className="away">
+            <td className="logo">
+              <img src={game.away.logo} />
+            </td>
             <td>{game.away.full_name}</td>
             <td style={scoreWidth}>{game.away.score}</td>
           </tr>
           <tr className="home">
+            <td className="logo">
+              <img src={game.home.logo} />
+            </td>
             <td>{game.home.full_name}</td>
             <td>{game.home.score}</td>
           </tr>
