e7b64b0fa36359153880bdfee69186431eb0cee9
Dan Weaver <daniel.weaver@gmail.com>
2018-09-29 10:52:22 -0600

    add pick access controls

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import GameReducer from './game_reducer';
 import PickSetReducer from './pick_set_reducer';
+import AccessReducer from './access_reducer';
 
 const rootReducer = combineReducers({
   games: GameReducer,
-  pickSets: PickSetReducer
+  pickSets: PickSetReducer,
+  access: AccessReducer
 });
 
 export default rootReducer;
